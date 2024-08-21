1c4ec058f180a23770f8b4ddf10ad5afe11b70f5
deepak1suyal <dsuyal800@gmail.com>
2024-08-21 13:12:40 +0530

    first commit

---
+++ prisma/migrations/20240821070745_init/migration.sql
@@ -0,0 +1,12 @@
+-- CreateEnum
+CREATE TYPE "TodoStatus" AS ENUM ('DONE', 'ACTIVE');
+
+-- CreateTable
+CREATE TABLE "Todo" (
+    "id" SERIAL NOT NULL,
+    "task" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Todo_pkey" PRIMARY KEY ("id")
+);
